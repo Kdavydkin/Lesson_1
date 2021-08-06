d90a1f2028dab33e3e4a56410b87ec470c0cb0c2
Kdavydkin <88538824+Kdavydkin@users.noreply.github.com>
2021-08-06 13:43:48 +0300

    Add files via upload

---
+++ JumpableRunnable.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesson_1;
+
+public interface JumpableRunnable {
+    int getMaxHeight();
+    void jump();
+
+    int getMaxLength();
+    void run();
+}
---
+++ Obstraclable.java
@@ -0,0 +1,6 @@
+package ru.geekbrains.lesson_1;
+
+interface Obstaclable {
+    boolean toRun(int maxLength);
+    boolean toJump(int maxHeight);
+}
